c8beb10257f8b77a49e4118fbcc390cd23082047
sam.mentink <samari.b.dj@gmail.com>
2016-06-21 14:44:27 +1200

    addind to date code how ever not working as yet

--- minesweeper.js
+++ minesweeper.js
@@ -1,4 +1,24 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
-function startGame () {
+var boardCells = document.getElementsByClassName('board')[0].children;
+
+function startGame (element) {
+    for (var i = 0; i < boardCells.length; i++) {
+    element.addListeners('click', element)
+    element.addEventListeners('hidden', showCell)
+    element.addEventListeners.preventDefault(markCell)
+}
+}
+
+function addListeners (element) {
+    evt.target.classList.hidden('click')
+}
+
+function showCell (evt) {
+ evt.target.classList.hidden('showCell')
+}
+
+function markCell (evt) {
+evt.preventDefault()
+evt.marked.Class.toggle ('markcell')
 }
