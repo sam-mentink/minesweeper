3f5107da3a120163f6870c538fdfa5af34f8622b
sam.mentink <samari.b.dj@gmail.com>
2016-06-22 14:49:50 +1200

    6.1 first part- nearly got it on my own have referenced armadine code to confirm and correct

--- minesweeper.js
+++ minesweeper.js
@@ -9,7 +9,7 @@ function startGame () {
 
   function addListeners (element) {
     element.addEventListener ('click', showCell)
-    element.addEventListener('contextmenu', markCell)
+    element.addEventListener( markCell)
     }
 
 
@@ -20,3 +20,17 @@ function showCell (evt) {
 function markCell (evt) {
   evt.preventDefault()
  }
+
+var board = {
+  cells: []
+}
+
+function getRow (element) {
+  var classy = element.classList
+  for (var i = 0; i < classy.length; i++) {
+    if (classy[i].substring === 'row' ) {
+      return Number(classes[i].split('-')[1])
+
+    }
+  }
+}
