85ea832785835dd22d6b40601881e38ae038cc78
sam.mentink <samari.b.dj@gmail.com>
2016-06-23 16:57:23 +1200

    6.1 complete not working how ever most code is near as run by teacher

--- minesweeper.js
+++ minesweeper.js
@@ -4,12 +4,13 @@ function startGame () {
   var boardCells = document.getElementsByClassName('board')[0].children
   for (var i = 0; i < boardCells.length; i++) {
     addListeners(boardCells[i])
+    addCellToBoard(cell[i])
   }
 }
 
   function addListeners (element) {
     element.addEventListener ('click', showCell)
-    element.addEventListener( markCell)
+    element.addEventListener(markCell)
     }
 
 
@@ -29,8 +30,30 @@ function getRow (element) {
   var classy = element.classList
   for (var i = 0; i < classy.length; i++) {
     if (classy[i].substring === 'row' ) {
-      return Number(classes[i].split('-')[1])
+      return Number(classy[i].split('-')[1])
 
     }
   }
 }
+
+function getCol (element) {
+  var classy = element.classList
+  for (var i = 0; i < classy.length; i++) {
+    if (classy[i].substring === 'col' ) {
+      return Number(classy[i].split('-')[1])
+
+    }
+  }
+}
+
+function addCellToBoard (cell) {
+  newCell = {
+    row: getRow(cell)
+    col: getCol
+    isMine: classList.contains('mine')
+
+    board.cells.push(newCell)
+  }
+}
+
+//el.classList.add("foo");
